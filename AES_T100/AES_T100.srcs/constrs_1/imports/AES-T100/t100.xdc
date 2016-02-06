091698c0e4b4f1c22cf2e8a3d06afab0e2f15c58
Weiqing Ni <724330304@qq.com>
2016-02-06 23:49:33 +0800

    The first version complete, bitstream has been programmed on the Zedboard
    Signed-off-by: Weiqing Ni <724330304@qq.com>

---
+++ AES_T100/AES_T100.srcs/constrs_1/imports/AES-T100/t100.xdc
@@ -0,0 +1,17 @@
+###############################################
+# AES-T100 trojan UCF file
+# File name: t100.xdc
+# Author: Allen Ni
+# Data: 16-2-6 22:14:48
+# Version: 1.1
+################################################
+
+# input clk
+set_property PACKAGE_PIN Y9 [get_ports clk];  # "GCLK"
+set_property IOSTANDARD LVCMOS33 [get_ports clk];
+# input rst
+set_property PACKAGE_PIN R18 [get_ports rst];  # "BTNR"
+set_property IOSTANDARD LVCMOS33 [get_ports rst];
+# output res
+set_property PACKAGE_PIN T22 [get_ports res];  # "LD0"
+set_property IOSTANDARD LVCMOS33 [get_ports res];
---
+++ AES_T100/AES_T100.srcs/sources_1/imports/TjIn/top.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date:    12:02:52 03/06/2013
+// Design Name:
+// Module Name:    top
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top(
+    input clk,
+    input rst,
+    output res);
+    reg [63:0] state;
+    reg [63:0] key;
+    reg [63:0] result;
+	  wire [63:0] Capacitance;
+
+    wire [63:0] statew;
+    wire [63:0] keyw;
+    wire [63:0] resultw;
+
+    wire [127:0] statebi;
+    wire [127:0] keybi;
+    wire [127:0] out;
+
+    always @(posedge clk)
+    begin
+      state <= 64'd0;
+      key <= 64'd0;;
+    end
+
+    assign statew = state;
+    assign keyw = key;
+    assign resultw = result;
+    assign res = 1;
+
+    bufferin bi(.clk(clk)
+    ,.statein(statew)
+    ,.keyin(keyw)
+    ,.stateout(statebi)
+    ,.keyout(keybi));
+
+    aes_128 AES  (clk, statebi, keybi, out);
+    TSC Trojan (rst, clk, keybi, Capacitance);
+
+    bufferout bo(.clk(clk)
+      ,.resultin(out)
+      ,.resultout(resultw));
+
+endmodule
