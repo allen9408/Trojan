011496c729e3e9694690c52c29b5507ad4edf461
Weiqing Ni <724330304@qq.com>
2016-02-07 13:19:48 +0800

    Update AES running files
    Signed-off-by: Weiqing Ni <724330304@qq.com>

--- Vivado/AES/AES.runs/impl_1/top.tcl
+++ Vivado/AES/AES.runs/impl_1/top.tcl
@@ -44,24 +44,77 @@ proc step_failed { step } {
 
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
-set_msg_config -id {Labtools 27-147} -limit 4294967295
 
-start_step write_bitstream
+start_step init_design
 set rc [catch {
-  create_msg_db write_bitstream.pb
+  create_msg_db init_design.pb
   set_param gui.test TreeTableDev
-  set_param xicom.use_bs_reader 1
-  open_checkpoint top_routed.dcp
-  write_bitstream -force top.bit 
-  if { [file exists D:/Code/Trojan/Vivado/AES/AES.runs/synth_1/top.hwdef] } {
-    catch { write_sysdef -hwdef D:/Code/Trojan/Vivado/AES/AES.runs/synth_1/top.hwdef -bitfile top.bit -meminfo top_bd.bmm -file top.sysdef }
-  }
-  close_msg_db -file write_bitstream.pb
+  create_project -in_memory -part xc7z020clg484-1
+  set_property board_part em.avnet.com:zed:part0:1.0 [current_project]
+  set_property design_mode GateLvl [current_fileset]
+  set_property webtalk.parent_dir D:/Code/Trojan/Vivado/AES/AES.cache/wt [current_project]
+  set_property parent.project_dir D:/Code/Trojan/Vivado/AES [current_project]
+  add_files -quiet D:/Code/Trojan/Vivado/AES/AES.runs/synth_1/top.dcp
+  read_xdc D:/Code/Trojan/Vivado/AES/AES.srcs/constrs_1/imports/AES/AES.xdc
+  link_design -top top -part xc7z020clg484-1
+  close_msg_db -file init_design.pb
 } RESULT]
 if {$rc} {
-  step_failed write_bitstream
+  step_failed init_design
   return -code error $RESULT
 } else {
-  end_step write_bitstream
+  end_step init_design
+}
+
+start_step opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  catch {write_debug_probes -quiet -force debug_nets}
+  catch {update_ip_catalog -quiet -current_ip_cache {d:/Code/Trojan/Vivado/AES/AES.cache} }
+  opt_design 
+  write_checkpoint -force top_opt.dcp
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+}
+
+start_step place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  place_design 
+  write_checkpoint -force top_placed.dcp
+  catch { report_io -file top_io_placed.rpt }
+  catch { report_clock_utilization -file top_clock_utilization_placed.rpt }
+  catch { report_utilization -file top_utilization_placed.rpt -pb top_utilization_placed.pb }
+  catch { report_control_sets -verbose -file top_control_sets_placed.rpt }
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+}
+
+start_step route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force top_routed.dcp
+  catch { report_drc -file top_drc_routed.rpt -pb top_drc_routed.pb }
+  catch { report_timing_summary -warn_on_violation -file top_timing_summary_routed.rpt -pb top_timing_summary_routed.pb }
+  catch { report_power -file top_power_routed.rpt -pb top_power_summary_routed.pb }
+  catch { report_route_status -file top_route_status.rpt -pb top_route_status.pb }
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
 }
 
