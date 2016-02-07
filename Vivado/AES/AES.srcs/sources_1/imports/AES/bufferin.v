6582dee61f7dcf62d650b970cc7a218f8063ee95
Weiqing Ni <724330304@qq.com>
2016-02-07 13:06:21 +0800

    AES algorithm project complete
    Signed-off-by: Weiqing Ni <724330304@qq.com>

---
+++ Vivado/AES/AES.srcs/sources_1/imports/AES/bufferin.v
@@ -0,0 +1,14 @@
+module bufferin(
+  input         clk,
+  input [64:0]  statein,
+  input [64:0]  keyin,
+  output reg [127:0] stateout,
+  output reg [127:0] keyout
+  );
+
+  always @(posedge clk)
+  begin
+    stateout <= {stateout[63:0], statein};
+    keyout <= {keyout[63:0], keyin};
+  end
+endmodule
