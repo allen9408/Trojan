ebb8b97e7cb8fa2194e9b7ed75d1c92d9e610b04
Weiqing Ni <724330304@qq.com>
2016-02-13 00:23:48 +0800

    Report power result in tcl script
    Signed-off-by: Weiqing Ni <724330304@qq.com>

--- Vivado/vivado.tcl
+++ Vivado/vivado.tcl
@@ -17,3 +17,7 @@ wait_on_run synth_1
 # run implementation and generate bitstream
 launch_runs impl_1 -to_step write_bitstream
 wait_on_run impl_1
+
+# report power
+open_run impl_1
+report_power -name {power_1} -hier all -xpe ./power/power$argv.xpe
