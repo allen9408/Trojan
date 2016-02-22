394729417d256555130076f568c9dc64905345b2
Weiqing Ni <724330304@qq.com>
2016-02-22 19:15:24 +0800

    Modify clear script
    Signed-off-by: Weiqing Ni <724330304@qq.com>

--- Vivado/clear.bat
+++ Vivado/clear.bat
@@ -1,6 +1,6 @@
 ::#########################################
 ::# Filename: clear.bat
-::# Usage: open project --> synthesis --> implementation --> generate bitstream
+::# Usage: clear type - *.v *.log *.tmp .Xil/
 ::# Developer: Allen Ni
 ::# Version: 1.0
 ::# Date: 2016.2.12
@@ -8,7 +8,26 @@
 ::clear
 @echo off
 
+if "%1" EQU "" goto noproj
+
+if not exist .\"%1" goto error
+
+::clear
+cd .\"%1"
+del .\*.v
 del *.log *.jou *.tmp
 rd /s /q .Xil
+cd ..
 
 @echo Complete!
+goto exit
+
+:error
+@echo Failed: Project not exist!
+goto exit
+
+:noproj
+@echo Failed: Please enter the project name!
+goto exit
+
+:exit
