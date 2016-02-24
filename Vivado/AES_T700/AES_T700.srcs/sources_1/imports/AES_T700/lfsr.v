592620324b56730c77ea5ea7300baf49207375a8
Weiqing Ni <724330304@qq.com>
2016-02-25 00:05:42 +0800

    Add AES-T700
    Signed-off-by: Weiqing Ni <724330304@qq.com>

---
+++ Vivado/AES_T700/AES_T700.srcs/sources_1/imports/AES_T700/lfsr.v
@@ -0,0 +1,26 @@
+
+// It implements X^20 + X^13 + X^9 + X^5 + 1
+module lfsr_counter (
+	input rst, clk, Tj_Trig,
+   output [19:0] lfsr
+	);
+
+	reg [19:0] lfsr_stream;
+	wire d0; 
+		
+	assign lfsr = lfsr_stream; 
+	assign d0 = lfsr_stream[15] ^ lfsr_stream[11] ^ lfsr_stream[7] ^ lfsr_stream[0]; 
+
+	always @(posedge clk)
+		if (rst == 1) begin
+			//lfsr_stream <= "10011001100110011001";
+			lfsr_stream <= 20'h99999;
+		end else begin
+			if (Tj_Trig == 1) begin
+				lfsr_stream <= {d0,lfsr_stream[19:1]}; 
+			end else begin
+				lfsr_stream <= lfsr_stream ;
+			end	
+		end
+		
+endmodule
