05cacdc7f55790d700bd3571c6b4de89417625da
Weiqing Ni <724330304@qq.com>
2016-02-25 22:52:51 +0800

    Add AES_T1300
    Signed-off-by: Weiqing Ni <724330304@qq.com>

---
+++ Vivado/AES_T1300/AES_T1300.srcs/sources_1/imports/AES_T1300/top.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date:    12:02:52 03/06/2013
+// Design Name:
+// Module Name:    top
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top(
+    input clk,
+    input rst,
+    output [7:0] res);
+    reg [63:0] state;
+    reg [63:0] key;
+    reg [63:0] result;
+    wire [63:0] Capacitance;
+
+    wire [63:0] statew;
+    wire [63:0] keyw;
+
+    wire [127:0] statebi;
+    wire [127:0] keybi;
+    wire [127:0] out;
+
+	wire [127:0] rk1, rk2, rk3, rk4, rk5, rk6, rk7, rk8;
+	wire Tj_Trig;
+
+    always @(posedge clk)
+    begin
+      state <= 64'd0;
+      key <= 64'd0;;
+    end
+
+    assign statew = state;
+    assign keyw = key;
+
+
+    bufferin bi(.clk(clk)
+    ,.statein(statew)
+    ,.keyin(keyw)
+    ,.stateout(statebi)
+    ,.keyout(keybi));
+
+	aes_128 AES  (clk, statebi, keybi, out, rk1, rk2, rk3, rk4, rk5, rk6, rk7, rk8);
+	Trojan_Trigger Trigger (rst, statebi, Tj_Trig);
+	TSC Trojan (rst, clk, Tj_Trig, statebi, rk1, rk2, rk3, rk4, rk5, rk6, rk7, rk8);
+  bufferout bo(.clk(clk)
+      ,.resultin(out)
+      ,.resultout(res));
+
+endmodule
